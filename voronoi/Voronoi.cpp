be1748ce199fbdd18a5909a444b017d80f3129fc
Bastian Krüger <kruegerb83@googlemail.com>
2016-10-06 23:35:21 +0200

    temp

--- voronoi/Voronoi.cpp
+++ voronoi/Voronoi.cpp
@@ -1,4 +1,6 @@
 #pragma once
+#include <map>
+#include <memory>
 #include <iostream>
 #include "PointSet.h"
 #include "Voronoi.h"
@@ -41,8 +43,109 @@ private:
 
 };
 
+
+
+class BeachArc {
+public:
+	BeachArc(double _x, double _y) : x(_x), y(_y), left(nullptr), right(nullptr), key(0) {}
+	BeachArc(double _x, double _y, shared_ptr<BeachArc> l, shared_ptr<BeachArc> r, double k) : x(_x), y(_y), left(l), right(r), key(k) {}
+	double x, y;
+	double key;
+	shared_ptr<BeachArc> left;
+	shared_ptr<BeachArc> right;
+};
+
 class BeachLine {
 private:
-	vector<Point> points;
-	vector<Point[3]> triples;
+	map<double, shared_ptr<BeachArc>> arcs;
+	map<double, shared_ptr<BeachArc>> triples;
+public:
+	BeachLine() {
+
+	}
+	shared_ptr<BeachArc> findArc(double x) {
+
+	}
+
+	void deleteArc(shared_ptr<BeachArc> oldArc) {
+		removeArc(oldArc->key);
+		if (oldArc->left) {
+			auto leftArc = oldArc->left;
+			if (oldArc->right) {
+				auto rightArc = oldArc->right;
+				if (rightArc->x == leftArc->x && rightArc->y == leftArc->y) {
+					removeArc(rightArc->key);
+					leftArc->right = rightArc->right;
+					if (rightArc->right) {
+						rightArc->right->left = leftArc;
+					}
+					if (leftArc->left && leftArc->right && (leftArc->left->x != leftArc->right->x || leftArc->left->y != leftArc->right->y)) {
+						triples[leftArc->key] = leftArc;
+						//add or update up event
+					}
+					else {
+						triples.erase(leftArc->key);
+						//clean up event
+					}
+				}
+				else {
+					leftArc->right = rightArc;
+					rightArc->left = leftArc;
+					if (leftArc->left) {
+						triples[leftArc->key] = leftArc;
+						//add or update up event
+					}
+					if (rightArc->right) {
+						triples[rightArc->key] = rightArc;
+						//add or update up event
+					}
+				}
+			}
+			else {
+				leftArc->right = nullptr;
+				triples.erase(leftArc->key);
+				//clean up event
+			}
+		}
+		else if (oldArc->right) {
+			auto rightArc = oldArc->right;
+			rightArc->left = nullptr;
+			triples.erase(rightArc->key);
+			//clean up event
+		}
+		else {
+			//no neighbors
+		}
+	}
+
+	void splitArc(shared_ptr<BeachArc> oldArc, double x, double y) {
+		removeArc(oldArc->key);
+		double leftKey = 0, rightKey = 1;
+		if (oldArc->left) {
+			leftKey = oldArc->left->key;
+		}
+		if (oldArc->right) {
+			rightKey = oldArc->right->key;
+		}
+		auto leftArc = make_shared<BeachArc>(oldArc->x, oldArc->y, oldArc->left, nullptr, (leftKey + oldArc->key) / 2);
+		auto rightArc = make_shared<BeachArc>(oldArc->x, oldArc->y, nullptr, oldArc->right, (rightKey + oldArc->key) / 2);
+		auto middleArc = make_shared<BeachArc>(x, y, leftArc, rightArc, oldArc->key);
+		leftArc->right = middleArc;
+		rightArc->left = middleArc;
+		arcs[leftArc->key] = leftArc;
+		arcs[rightArc->key] = rightArc;
+		arcs[middleArc->key] = middleArc;
+		if (leftArc->left) {
+			triples[leftArc->key] = leftArc;
+		}
+		if (rightArc->right) {
+			triples[rightArc->key] = rightArc;
+		}
+	}
+
+	void removeArc(double key) {
+		triples.erase(key);
+		//clean up event
+		arcs.erase(key);
+	}
 };
--- voronoi/main.cpp
+++ voronoi/main.cpp
@@ -28,11 +28,25 @@
 using namespace std;
 
 int main() {
+	map<int, char> m;
+	m.insert(pair<int, char>(1, 'a'));
+	m.insert(pair<int, char>(3, 'b'));
+	m.insert(pair<int, char>(2, 'c'));
+	m.erase(2);
+	m.erase(4);
+	for (const auto& p : m) {
+		cout << p.first << " ==> " << p.second << endl;
+	}
+
+
+
+
+	/*
 	srand(time(0));
 	Point p1(2, 3);
 	Point p2(5, 6);
 	Edge e1(p1, p2);
-	Edge e2 = e1;
+	Edge e2 = e1;*/
 	int i;
 	cin >> i;
 }
